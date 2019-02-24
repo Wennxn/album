d901fa0cc974e6bb9055363cbec64e57b6ceca39
Huang Wen <huangw@Huangs-MacBook-Air.local>
2019-02-24 15:02:56 -0800

    added card and card section, implemented the album information and added styles

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,13 +1,60 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
+import Card from './Card'
+import CardSection from "./CardSection"
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({album}) => {
+    // destructure album and style
+    const { title, artist, thumbnail_image, image } = album;
+    const { headerContentStyle, imageStyle, imageContainerStyle, headerTextStyle, albumImageStyle } = styles;
+
     return(
-        <View>
-            <Text>{props.album.title}</Text>
-        </View>
+        <Card>
+            <CardSection>
+                <View style={imageContainerStyle}>
+                    <Image 
+                        style={imageStyle}
+                        source={{ uri: thumbnail_image }}
+                    />
+                </View>
+                <View style={headerContentStyle}>
+                    <Text style={headerTextStyle}>{ title }</Text>
+                    <Text>{ artist }</Text>
+                </View>
+            </CardSection>
+            <CardSection>
+                <Image 
+                    style={albumImageStyle}
+                    source={{uri: image}}
+                />
+            </CardSection>
+        </Card>
     );
 };
 
+const styles = {
+    headerContentStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around'
+    },
+    headerTextStyle: {
+        fontSize: 18
+    },
+    imageStyle: {
+        height: 50,
+        width: 50,
+    },
+    imageContainerStyle: {
+        justifyContent: 'center',
+        alignItem: 'center',
+        marginLeft: 10,
+        marginRight: 10
+    },
+    albumImageStyle: {
+        height: 300,
+        flex: 1,
+        width: null
+    }
+}
 
 export default AlbumDetail;
